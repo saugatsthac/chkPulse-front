fb9d592f7d508cd5aef9d43c0ca555f1f9770b32
saugatsthax <saugatsthax@outlook.com>
2026-06-12 15:54:54 +0545

    fix: simplify layout structure and improve button alignment in ProjectAndUrls component

--- src/Components/ProjectAndUrls.jsx
+++ src/Components/ProjectAndUrls.jsx
@@ -13,90 +13,90 @@ export default function ProjectAndUrls({ activeProject, projects, activeProjectD
     return (
         <div className="w-80 border-slate-800 pr-0 text-white/90 h-full flex flex-col gap-2 bg-blue-900/10 font-light">
             {/* <img src='/assets/ChatGPT Image Jun 11, 2026, 08_23_03 PM.png' /> */}
-            <div className="w-full flex flex-col items-start text-lg p-4 gap-3">
-                <div className="flex flex-col items-start w-full">
-                    <div className='w-full flex h-auto gap-2 py-8 pt-4 p-4 justify-center'>
-                        {/* <img src={logo} alt='logo' /> */}
-                        <img src={monitor} alt='monitor' className='w-1/5' />
-                        <img src={name} alt='name' className='w-1/2' />
-
-                    </div>
-                    <button className='flex gap-2'
-                        onClick={() => {
-                            setSidebarSelection('dashboard')
-                        }}>
-                        <DashboardIcon />
-                        <span>
-
-                            Dashboard
-                        </span>
-                    </button>
-                    <button className='flex gap-2'
-                        onClick={() => {
-                            setSidebarSelection('monitors')
-                        }}>
-                        <MonitorIcon />
-                        <span>
-                            Monitors
-                        </span>
-                    </button>
-                    <button className='flex gap-2'>
-                        <NotificationsNoneIcon />
-                        <span>
-                            Incidents
-                        </span>
-                    </button>
-                    <button className='flex gap-2'>
-                        <MonitorHeartIcon />
-                        <span>
-                            Status Pages
-
-                        </span>
-                    </button>
+            <div className="flex flex-col items-start w-full p-4">
+                <div className='w-full flex h-auto gap-2 py-8 pt-4 p-4 justify-center'>
+                    {/* <img src={logo} alt='logo' /> */}
+                    <img src={monitor} alt='monitor' className='w-1/5' />
+                    <img src={name} alt='name' className='w-1/2' />
 
                 </div>
+                <button className='flex gap-2'
+                    onClick={() => {
+                        setSidebarSelection('dashboard')
+                    }}>
+                    <DashboardIcon />
+                    <span>
 
+                        Dashboard
+                    </span>
+                </button>
+                <button className='flex gap-2'
+                    onClick={() => {
+                        setSidebarSelection('monitors')
+                    }}>
+                    <MonitorIcon />
+                    <span>
+                        Monitors
+                    </span>
+                </button>
+                <button className='flex gap-2'>
+                    <NotificationsNoneIcon />
+                    <span>
+                        Incidents
+                    </span>
+                </button>
+                <button className='flex gap-2'>
+                    <MonitorHeartIcon />
+                    <span>
+                        Status Pages
 
-                <div className='flex flex-col'>
-                    <h2 className="text-xs mb-1">PROJECTS</h2>
-                    <div className="
-            flex flex-col 
-            rounded-tl-xl rounded-br-xl 
+                    </span>
+                </button>
+
+            </div>
+
+            {/* rounded-tl-xl 
+                    rounded-br-xl  */}
+            <div className="w-full grow flex flex-col items-start text-lg p-4 pr-0 pb-0 min-h-0">
+
+                {/* <div className='flex flex-col w-full'> */}
+                <h2 className="text-xs mb-1">PROJECTS</h2>
+                <div className="
+            flex flex-col
             w-full scrollbar-thin overflow-y-auto 
-            pr-2.5
             [scrollbar-gutter:stable] 
             scrollbar-track-transparent
             scrollbar-thumb-slate-800/50 scrollbar-thumb-rounded-lg
             hover:scrollbar-thumb-slate-800/60">
-                        {console.log('print', projects)}
-                        {projects?.map((p) => (
-                            <div
-                                key={p._id}
-                                onClick={() => { setActiveProjectData(p) }}
-                                className={`flex flex-col
+                    {console.log('print', projects)}
+                    {projects?.map((p) => (
+                        <div
+                            key={p._id}
+                            onClick={() => { setActiveProjectData(p) }}
+                            className={`flex flex-col
                             cursor-pointer transition
                             `}
-                            >
-                                <span className="text-lg">
-                                    {p.name}
-                                </span>
-                                <span className="text-sm text-white/80 w-full text-right">
-                                    {p.description}
-                                </span>
-                            </div>
-                        ))}
-                    </div>
-                    <button className="font-light bg-none
+                        >
+                            <span className="text-lg">
+                                {p.name}
+                            </span>
+                            <span className="text-sm text-white/80 w-full text-right">
+                                {p.description}
+                            </span>
+                        </div>
+                    ))}
+                </div>
+                <button className="font-light bg-none
             flex w-fit text-lg 
             transition-all duration-300"
-                        onClick={() => {
-                            setShowModal(true);
-                            setModalType("createProject");
+                    onClick={() => {
+                        setShowModal(true);
+                        setModalType("createProject");
 
-                        }}>
-                        Create Project
-                    </button>
-                </div>
+                    }}>
+                    Create Project
+                </button>
+                {/* </div> */}
             </div>
             <div className="border-t border-white/10 p-4 mt-auto">
                 <button className="font-light bg-none
